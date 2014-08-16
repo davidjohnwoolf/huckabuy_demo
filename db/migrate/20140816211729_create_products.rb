79222051ac0c2b9eb62a559cc2bd7fd6e2a36e62
David Woolf <davidjohnwoolf@gmail.com>
2014-08-16 17:48:11 -0600

    create product attributes and migrate

--- db/migrate/20140816211729_create_products.rb
+++ db/migrate/20140816211729_create_products.rb
@@ -1,7 +1,14 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.string :brand
+      t.string :category
+      t.string :description
+      t.string :manufacturer
+      t.string :name
+      t.string :price
+      t.string :upc
+      t.string :images, array: true, default: '{}'
       t.timestamps
     end
   end
